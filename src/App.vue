2ea9efdcb8c9c7cceef59f0e992c77b4e6fb93ba
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-04-20 12:22:38 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HeaderBoolfix />
+  </div>
+</template>
+
+<script>
+import HeaderBoolfix from './components/HeaderBoolfix.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderBoolfix,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HeaderBoolfix.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <img class="h-50" alt="Vue logo" src="@/assets/logo.png">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello img{
+  height: 80px;
+}
+</style>
