abbad1d7b21e108c198e774a1e8b592f38a55e18
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-04-21 19:15:57 +0200

    creato componente per film e serie

---
+++ src/components/FilmBoolfix.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="row justify-content-center gap-3 text-light">
+        <div class="col-2 fs-4 p-1 border border-2 m-2"  v-for="item in films" :key="item.id">
+            <img class="w-100" :src="`http://image.tmdb.org/t/p/original${item.poster_path}`"
+                @error="imageNotFound"
+                alt=""
+            >
+            <p class="fs-6">TITOLO: {{item.title}}</p>
+            <p class="fs-6">TITOLO ORIGINALE: {{ item.original_title }}</p>
+            <img class="flag" :src="showFlags (item.original_language)" 
+                @error="imageNotFound"
+            >
+            <p class="fs-6">VOTO:  <font-awesome-icon icon="fa-solid fa-star" class="text-warning"/> {{ item.vote_average }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"FilmBoolfix",
+    props:{
+        films: Array,
+    },
+        methods:{
+        showFlags(flag){
+            if( flag === "ja"){
+                return "https://www.kidlink.org/icons/f0-jp.gif";
+            }else if ( flag === "en"){
+                return "https://www.kidlink.org/icons/f0-gb.gif";
+            }
+            return `https://www.kidlink.org/icons/f0-${flag}.gif`;
+
+        },
+
+        imageNotFound(error){
+            error.target.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8uia93jUhtlaNB5s9-xnmhh8tek0NsQ0BMg&usqp=CAU"
+        },
+
+    }
+
+}
+</script>
+
+<style lang='scss' scoped>
+.col-2{
+    background-color: rgba($color: #9e9e9e, $alpha: 0.3);
+};
+.flag{
+    width: 20px
+
+};
--- src/components/MainBoolfix.vue
+++ src/components/MainBoolfix.vue
@@ -1,56 +1,34 @@
 <template>
-<main class="container-fluid">
-    <div class="row flex-wrap text-light justify-content-evenly">
-        <h1 class="bg-dark text-center mb-0 p-2 border-light border-bottom">FILM</h1>
-        <div class="col-2 fs-4 p-1 border border-2 m-2" v-for="item in films" :key="item.id">
-            <img class="poster" :src="`http://image.tmdb.org/t/p/original${item.poster_path}`" alt="">
-            <p class="fs-6">TITOLO: {{item.title}}</p>
-            <p class="fs-6">TITOLO ORIGINALE: {{ item.original_title }}</p>
-            <img :src="showFlags (item.original_language)">
-            <p class="fs-6">VOTO: {{ item.vote_average }}</p>
-        </div>
-        <h1 class="bg-dark text-center p-2  border-light border-bottom">SERIE</h1>
-        <div class="col-2 fs-4 p-1 border border-2 m-2" v-for="item in series" :key="item.id">
-            <img class="poster" :src="`http://image.tmdb.org/t/p/original${item.poster_path}`" alt="">
-            <p class="fs-6">TITOLO: {{item.name}}</p>
-            <p class="fs-6">TITOLO ORIGINALE: {{ item.original_name }}</p>
-            <img :src="showFlags (item.original_language)">
-            <p class="fs-6">VOTO: {{ item.vote_average }}</p>
-        </div>
+<main>
+    <div class="container-fluid">
+        <h1 class="bg-dark text-center mb-0 p-2 border-light border-bottom text-light">FILM</h1>
+        <FilmBoolfix :films="films" />
+        <h1 class="bg-dark text-center p-2  border-light border-bottom text-light">SERIE</h1>
+        <SerieBoolfix :series="series" />
     </div>
+
 </main>
 
 </template>
 
 <script>
+import FilmBoolfix from "./FilmBoolfix.vue";
+import SerieBoolfix from "./SerieBoolfix.vue";
 
 export default {
     name:"MainBoolfix",
+    components:{
+        FilmBoolfix,
+        SerieBoolfix,
+    },
     props:{
         films: Array,
         series:Array,
     },
-    methods:{
-        showFlags(flag){
-            if( flag === "ja"){
-                return "https://www.kidlink.org/icons/f0-jp.gif";
-            }else if ( flag === "en"){
-                return "https://www.kidlink.org/icons/f0-gb.gif";
-            }
-            return `https://www.kidlink.org/icons/f0-${flag}.gif`;
-
-        },
-
-    }
 };
 </script>
 
 <style lang='scss' scoped>
-.col-2{
-    background-color: rgba($color: #9e9e9e, $alpha: 0.3);
-};
-.poster{
-    width: 100%;
-}
+
 
 </style>
