922087d89a1205d49e1bd8cdd7e2a1629209ad93
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-04-23 12:13:31 +0200

    milestone 4 completata per componente serie

--- src/components/SerieBoolfix.vue
+++ src/components/SerieBoolfix.vue
@@ -1,25 +1,34 @@
 <template>
     <div class="row justify-content-center me-0 gap-2 text-light">
-        <div class="col-2 bg_lv fs-4 p-1 border border-2 m-2" v-for="item in series" :key="item.id" >
-            <img class="w-100" :src="`http://image.tmdb.org/t/p/original${item.poster_path}`"
-             @error="imageNotFound"
-             alt="">
-            <p class="fs-6">TITOLO: {{item.name}}</p>
-            <p class="fs-6">TITOLO ORIGINALE: {{ item.original_name }}</p>
-            <img class="flag d-block" :src="showFlags (item.original_language)"
-                @error="imageNotFound"
-            >
-            <div class="fs-6 d-inline-block" v-for="(n, index) in 5" :key="index">
-                <font-awesome-icon
-                 :class="n <= item.vote_average / 2 ? 'text-warning' : ''"
-                 :icon="
-                    item.vote_average / 2 == 0
-                  ? 'fa-regular fa-star'
-                  : 'fa-solid fa-star'
-                 "
-                />
+        <div class="col-3 bg_lv fs-4 p-1 border border-2 m-2" v-for="item in series" :key="item.id">
+                <img class=" image" :src="`http://image.tmdb.org/t/p/original${item.poster_path}`"
+                 @error="imageNotFound"
+                 alt=""
+                 > 
+            <div class="overlay">
+                <div class="text fs_10 ">
+                    <p class="fs_10 m-1">TITOLO: {{item.name}}</p>
+                    <p class="fs_10 m-1">TITOLO ORIGINALE: {{ item.original_name }}</p>
+                    <div class="container-flag  d-flex justify-content-center">
+                        <img class="flag d-block" :src="showFlags (item.original_language)" 
+                         @error="imageNotFound"
+                        >
+                    </div>
+                    <div class="fs_10 d-inline-block" v-for="(n, index) in 5" :key="index">
+                        <font-awesome-icon
+                            :class="n <= item.vote_average / 2 ? 'text-warning' : ''"
+                            :icon="
+                            item.vote_average / 2 == 0
+                            ? 'fa-regular fa-star'
+                            : 'fa-solid fa-star'
+                            "
+                        />
+                    </div>
+                    <p class="fs_10 m-100 w-100">DESCRIZIONE:  {{item.overview}}</p>
+
+                </div>
             </div>
-        </div>  
+        </div>
     </div>
 </template>
 
@@ -29,7 +38,7 @@ export default{
     props:{
         series:Array,
     },
-        methods:{
+     methods:{
         showFlags(flag){
             if( flag === "ja"){
                 return "https://www.kidlink.org/icons/f0-jp.gif";
@@ -50,6 +59,46 @@ export default{
 </script>
 
 <style lang='scss' scoped>
+
+.col-3 {
+  position: relative;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: black;
+}
+
+.col-3:hover .overlay {
+  opacity: 1;
+}
+
+.text {
+  color: white;
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+
 .bg_lv{
     background-color: rgba($color: #9e9e9e, $alpha: 0.3);
     cursor: pointer;
@@ -59,4 +108,7 @@ export default{
     width: 20px
 
 };
+.fs_10{
+    font-size: 10px;
+}
 </style>
